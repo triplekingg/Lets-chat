17e2b6d0ec13c8116d5ef60e088e0ea776fe2f96
triplekingg <tripleking123@gmail.com>
2023-01-22 16:04:58 +0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import './style.scss'
 
 const App = () => {
   return (
-    <Login />
+    <Home />
   )
 }
 
