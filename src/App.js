a5d702f9e837b223dfad813c6e23fd9e85b1861f
triplekingg <tripleking123@gmail.com>
2023-01-22 16:00:18 +0700

    add css and load resources

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Register from './pages/Register'
+import './style.scss'
 
 const App = () => {
   return (
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import Add from "../img/addAvatar.png"
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='formContainer'>
+        <div className='formWrapper'>
+            <span className='logo'>Lets Chat</span>
+            <span className='title'>Register</span>
+            <form>
+                <input type="text" placeholder='display name'/>
+                <input type="email" placeholder='email'/>
+                <input type="password" placeholder='passworc'/>
+                <input style={{display:"none"}} type="file" id='file' />
+                <label htmlFor='file'>
+                    <img src={Add} alt="" />
+                    <span>Add an avatar</span>
+                </label>
+                <button>Sign up</button>
+            </form>
+            <p>Do you have an account? Login</p>
+        </div>
+    </div>
   )
 }
 
