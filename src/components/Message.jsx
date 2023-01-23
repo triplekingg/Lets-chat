7155bb2fd3c4110253e52c2f726ff148dc154220
triplekingg <tripleking123@gmail.com>
2023-01-23 16:12:30 +0700

    finish designing

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+        <div className="messageInfo">   
+          <img
+           src="https://images.pexels.com/photos/12461870/pexels-photo-12461870.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" 
+           alt="" 
+          />
+          <span>just now</span>
+        </div>
+        <div className="messageContent">
+          <p>hello</p>
+          <img src="https://images.pexels.com/photos/12461870/pexels-photo-12461870.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+        </div>
+      </div>
   )
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,8 +8,9 @@ const Search = () => {
       </div>
       <div className="userChat">
         <img src="https://images.pexels.com/photos/12461870/pexels-photo-12461870.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
-        <div className="userChatInfo"></div>
+        <div className="userChatInfo">
         <span>Jane</span>
+        </div>
       </div>
     </div>
   )
