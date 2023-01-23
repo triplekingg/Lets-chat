2c984e218893f5589e755c3859f37fde77d347e0
triplekingg <tripleking123@gmail.com>
2023-01-23 17:35:49 +0700

    use router dom to manage routes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import './style.scss'
+import{
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <Register />
+    <BrowserRouter>
+      <Routes path="/">
+        <Route index element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+      </Routes> 
+    </BrowserRouter>  
   )
 }
 
