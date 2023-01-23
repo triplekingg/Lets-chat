2c6bf703cab467249e7f6f6d8fc5b8b92d231c29
triplekingg <tripleking123@gmail.com>
2023-01-23 15:37:49 +0700

    add styling and make changes to chat component

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/12461870/pexels-photo-12461870.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+        <div className="userChatInfo">
+        <span>Jane</span>
+        <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/12461870/pexels-photo-12461870.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+        <div className="userChatInfo">
+        <span>Jane</span>
+        <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/12461870/pexels-photo-12461870.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+        <div className="userChatInfo">
+        <span>Jane</span>
+        <p>Hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className="searchForm">
+        <input type="text" placeholder='Find a user'/>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/12461870/pexels-photo-12461870.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" />
+        <div className="userChatInfo"></div>
+        <span>Jane</span>
+      </div>
+    </div>
   )
 }
 
