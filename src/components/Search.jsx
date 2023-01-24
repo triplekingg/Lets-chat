3810ca7ce40d3b26ddb561f09fe2f2f38276a4fc
triplekingg <tripleking123@gmail.com>
2023-01-24 10:18:58 +0700

    trigger creation of collection when searching for user in the searchbar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import {collection,query, where, getDocs,} from "firebase/firestore";
+import React, { useContext, useState } from 'react'
+import {collection,query, where, getDocs, setDoc, updateDoc, serverTimestamp, getDoc, doc} from "firebase/firestore";
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 
@@ -8,6 +8,8 @@ const Search = () => {
   const [username, setUsername] = useState("")
   const [user, setUser] = useState(null)
   const [err, setErr] = useState(false)
+
+  const {currentUser} = useContext(AuthContext);
 
   const handleSearch = async () => {
     const q = query(
@@ -29,6 +31,44 @@ const Search = () => {
     e.code === "Enter" && handleSearch();
   };
 
+  const handleSelect = async()=>{
+    //check whether the group(chats in firestore) exists, if not create new one
+    const combinedId = currentUser.uid > user.uid
+      ? currentUser.uid + user.uid
+      : user.uid + currentUser.uid;
+      try{
+        const res = await getDoc(doc(db,'chats', combinedId));
+
+        if(!res.exists()){
+          //create a chat in chats collection
+          await setDoc(doc(db, 'chats', combinedId),{messages:[]});
+
+          //create user chats
+          await updateDoc(doc(db, "userChats", currentUser.uid), {
+            [combinedId + ".userInfo"]: {
+              uid: user.uid,
+              displayName: user.displayName,
+              photoURL: user.photoURL,
+            },
+            [combinedId + ".date"]: serverTimestamp(),
+          });
+  
+          await updateDoc(doc(db, "userChats", user.uid), {
+            [combinedId + ".userInfo"]: {
+              uid: currentUser.uid,
+              displayName: currentUser.displayName,
+              photoURL: currentUser.photoURL,
+            },
+            [combinedId + ".date"]: serverTimestamp(),
+          });
+        }
+      }catch(err){
+
+      }
+      setUser(null);
+      setUsername('');
+
+  }
   return (
     <div className="search">
       <div className="searchForm">
@@ -42,7 +82,7 @@ const Search = () => {
       </div>
       {err && <span>User not found!</span>}
       {user && (
-        <div className="userChat">
+        <div className="userChat" onClick={handleSelect}>
           <img src={user.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{user.displayName}</span>
